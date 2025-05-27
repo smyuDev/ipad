ac1566c96f738fc77f029127f6a47cfb2a2245b0
smyuDev <spc4yoo@gmail.com>
2025-05-27 19:01:02 +0900

    add: 검색 바 애니메이션 추가

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// 장바구니
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket');
+
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+  if (basketEl.classList.contains('show')) {
+    // hide
+    hideBasket();
+  } else {
+    // show
+    showBasket();
+  }
+});
+basketEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+});
+
+window.addEventListener('click', function () {
+  hideBasket();
+});
+
+function showBasket() {
+  basketEl.classList.add('show');
+}
+function hideBasket() {
+  basketEl.classList.remove('show');
+}
+
+
+// 검색
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; //전개연산자 얕은복사
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStaterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStaterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+  headerEl.classList.add('searching');
+  document.documentElement.classList.add('fixed');
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+  });
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
+}
+function hideSearch() {
+  headerEl.classList.remove('searching');
+  document.documentElement.classList.remove('fixed');
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + 's';
+  });
+  searchDelayEls.reverse().forEach((el, index) => {
+		el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + 's';
+  });
+  searchDelayEls.reverse();
+}
